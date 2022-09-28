8604a770cba32b60c21cbc3cbf7fe8e5081a468e
Rafi-Riday <rafi.riday@gmail.com>
2022-09-28 23:30:57 +0600

    added and configured react-toastify

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div>
       <div className="text-center text-warning">New Starting Page</div>
+      <button className='btn btn-info' onClick={() => toast("Wow so easy!")}>Notify!</button>
+      <ToastContainer icon={true} position="bottom-right" autoClose={3000} hideProgressBar={false} newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
     </div>
   );
 }
