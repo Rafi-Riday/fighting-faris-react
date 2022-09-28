fbf549946cf6e1700b6095aecc35c1cdc278af52
Rafi-Riday <rafi.riday@gmail.com>
2022-09-29 01:03:23 +0600

    added and configured font-awesome

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
+
   return (
     <div>
       <div className="text-center text-warning">New Starting Page</div>
-      <button className='btn btn-info' onClick={() => toast("Wow so easy!")}>Notify!</button>
-      <ToastContainer icon={true} position="bottom-right" autoClose={3000} hideProgressBar={false} newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+      <button className='btn btn-info' onClick={() => toast.info('Wow so easy!', { theme: 'colored', position: "bottom-right", autoClose: 3000, hideProgressBar: false, closeOnClick: true, pauseOnHover: true, draggable: true, progress: undefined, })}><FontAwesomeIcon icon={faCoffee} /></button>
+      <ToastContainer />
     </div>
   );
 }
